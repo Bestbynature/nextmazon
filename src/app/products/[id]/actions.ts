bb0e64c2310b158c81a8dede7236a3ba8d9236f3
Dammy <bestbynature2010@gmail.com>
2023-02-18 11:05:52 +0100

    Update Product details component

--- src/app/products/[id]/actions.ts
+++ src/app/products/[id]/actions.ts
@@ -5,14 +5,39 @@ import { prisma } from '@/lib/db/prisma';
 import { revalidatePath } from 'next/cache';
 
 const IncrementProductQuantity = async (productId: string) => {
-  const cart = await getCart() ?? (await createCart());
+  const cart = (await getCart()) ?? (await createCart());
 
   const articleInCart = cart?.items.find((item) => item.productId === productId);
 
-  if (articleInCart ) await prisma.cartItem.update({ where: { id: articleInCart.id }, data: { quantity: articleInCart.quantity + 1 } })
-  else await prisma.cartItem.create({ data: { cartId: cart!.id, productId, quantity: 1 } });
+  if (articleInCart)
+  await prisma.cart.update({
+    where: { id: cart!.id },
+    data: {
+      items: {
+        update: {
+          where: { id: articleInCart.id },
+          data: { quantity: articleInCart.quantity + 1 },
+        },
+      },
+    },
+  })
+    // await prisma.cartItem.update({
+    //   where: { id: articleInCart.id },
+    //   data: { quantity: articleInCart.quantity + 1 },
+    // });
+  else await prisma.cart.update({
+    where: { id: cart!.id },
+    data: {
+      items: {
+        create: {
+          productId,
+          quantity: 1,
+        },
+      },
+    },
+  }) // await prisma.cartItem.create({ data: { cartId: cart!.id, productId, quantity: 1 } });
 
-  revalidatePath("/products/[id]")
+  revalidatePath('/products/[id]', 'page');
 };
 
 export default IncrementProductQuantity;
