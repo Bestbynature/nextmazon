952e29b835e759924482660bb08d97375a0556f4
Dammy <bestbynature2010@gmail.com>
2023-03-07 15:55:29 +0100

    Update error component

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,10 +1,7 @@
-"use client"
-
+'use client';
 
 const ErrorPage = () => {
-  return (
-    <div>Something has gone wrong. Please refresh.</div>
-  )
-}
+  return <div>Something has gone wrong. Please refresh to resolve.</div>;
+};
 
-export default ErrorPage
+export default ErrorPage;
