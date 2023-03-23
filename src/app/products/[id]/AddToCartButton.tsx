ef5bee5784309683c6a7463eabb3687bd6fb1242
Dammy <bestbynature2010@gmail.com>
2023-03-23 15:42:30 +0100

    Update the button containing  server action that add items to cart

--- src/app/products/[id]/AddToCartButton.tsx
+++ src/app/products/[id]/AddToCartButton.tsx
@@ -5,19 +5,21 @@ import { AddShoppingCart } from '@mui/icons-material';
 import { useState, useTransition } from 'react';
 
 const AddToCartButton = ({ productId, incrementProductQuantity }: AddToCartButtonProps) => {
-  
   const [isPending, startTransition] = useTransition();
-  const [success , setSuccess] = useState(false);
-  
+  const [success, setSuccess] = useState(false);
+
   return (
     <div className="flex items-center gap-2">
-      <button className="btn btn-secondary" onClick={() => {
-        setSuccess(false);
-        startTransition(async () => {
-          await incrementProductQuantity(productId);
-          setSuccess(true);
-        });
-      }}>
+      <button
+        className="btn btn-secondary"
+        onClick={() => {
+          setSuccess(false);
+          startTransition(async () => {
+            await incrementProductQuantity(productId);
+            setSuccess(true);
+          });
+        }}
+      >
         Add To cart
         <AddShoppingCart />
       </button>
