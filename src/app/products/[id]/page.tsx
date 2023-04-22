bd74001e77ce9ea94265a296026458741190980c
Dammy <bestbynature2010@gmail.com>
2023-04-22 15:40:41 +0100

    Update product details page

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,18 +1,17 @@
 import NotFound from '@/app/not-found';
 import { prisma } from '@/lib/db/prisma';
-import { Product, ProductPageProps } from '@/lib/types/types';
 import Image from 'next/image';
 import PriceTag from '@/components/PriceTag';
-import { Metadata } from "next"
+import { Metadata } from 'next';
+import { cache } from 'react';
+import AddToCartButton from './AddToCartButton';
+import IncrementProductQuantity from './actions';
 
-export const getProduct = async (id: string) => {
-  const product = await prisma.product.findUnique({
-    where: { id }
-  });
-  return product;
-};
-
-export async function generateMetadata({ params: { id } }: ProductPageProps): Promise<Metadata> {
+export async function generateMetadata({
+  searchParams: { id },
+}: {
+  searchParams: { id: string };
+}): Promise<Metadata> {
   const product = await getProduct(id);
 
   return {
@@ -27,102 +26,48 @@ export async function generateMetadata({ params: { id } }: ProductPageProps): Pr
           alt: product?.name,
         },
       ],
-    }
-  }
+    },
+  };
 }
 
-export default function ProductPage({ product }: {product: Product}) {
-  if (product) {
-    return (
-      <div className='flex flex-col lg:flex-row gap-4 lg:items-center'>
-        <Image
-          src={product.imageUrl}
-          alt={product.name}
-          width={500}
-          height={500}
-          priority
-          className='rounded-lg'
-        />
+const getProduct = cache(async (id: string) => {
+  const product = await prisma.product.findUnique({
+    where: { id },
+  });
+  return product;
+});
 
-        <div className='flex flex-col justify-center items-center lg:items-start'>
-          <h1 className='text-5xl font-bold'>{product.name}</h1>
-          <PriceTag price={product.price} className='mt-4' />
-          <p className='text-xl py-6 font-semibold'>{product.description}</p>
-          This is the details page for {product.name} with id {product.id}
+export default function ProductPage({ searchParams: { id } }: { searchParams: { id: string } }) {
+  const loadProduct = async () => {
+    const product = await getProduct(id as string);
+
+    if (product) {
+      return (
+        <div className="flex flex-col lg:flex-row gap-4 lg:items-center">
+          <Image
+            src={product.imageUrl}
+            alt={product.name}
+            width={500}
+            height={500}
+            priority
+            className="rounded-lg"
+          />
+
+          <div className="flex flex-col justify-center items-center lg:items-start">
+            <h1 className="text-5xl font-bold">{product.name}</h1>
+            <PriceTag price={product.price} className="mt-4" />
+            <p className="text-xl py-6 font-semibold">{product.description}</p>
+            <AddToCartButton
+              productId={product.id}
+              incrementProductQuantity={IncrementProductQuantity}
+            />
+          </div>
         </div>
-      </div>
-    );
-  } else {
-    return <NotFound />;
-  }
+      );
+    } else {
+      return <NotFound />;
+    }
+  };
+
+  return <>{loadProduct()}</>;
 }
-
-
-// import NotFound from '@/app/not-found';
-// import { prisma } from '@/lib/db/prisma';
-// import { Product, ProductPageProps } from '@/lib/types/types';
-// import { cache } from 'react'
-// import Image from 'next/image'
-// import PriceTag from '@/components/PriceTag';
-// import { Metadata } from "next"
-
-// // const getProduct = cache(async (id: string) => {
-// //   const product = await prisma.product.findUnique({
-// //     where: {id}
-// //   })
-// //   return product
-// // })
-
-// export const getProduct = async (id: string) => {
-//   const product = await prisma.product.findUnique({
-//     where: { id }
-//   });
-//   return product;
-// };
-
-
-// export async function generateMetadata({params: {id}}: ProductPageProps): Promise<Metadata> {
-//   const product = await getProduct(id)
-
-//   return {
-//     title: product?.name + ' | Next.js Ecommerce',
-//     description: product?.description,
-//     openGraph: {
-//       images: [
-//         {
-//           url: product?.imageUrl || '',
-//           width: 500,
-//           height: 500,
-//           alt: product?.name,
-//         },
-//       ],
-//     }
-//   }
-// }
-
-//   export default function ProductPage({ product }: { product: Product }) {
-//   if (product) {
-//     return (
-//       <div className='flex flex-col lg:flex-row gap-4 lg:items-center'>
-//         <Image
-//         src={product.imageUrl} 
-//         alt={product.name} 
-//         width={500} 
-//         height={500}
-//         priority
-//         className='rounded-lg'
-//          />
-
-//         <div className='flex flex-col justify-center items-center lg:items-start'>
-//           <h1 className='text-5xl font-bold'>{product.name}</h1>
-//           <PriceTag price={product.price} className='mt-4'/>
-//           <p className='text-xl py-6 font-semibold'>{product.description}</p>
-//           This is the details page for {product.name} with id {product.id}
-//         </div>
-//       </div>
-//     );
-//   } else {
-//     return <NotFound />
-//   }
-
-// }
