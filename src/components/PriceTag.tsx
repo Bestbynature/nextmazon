442c4749eb177e04ae0a5da44661a70254725137
Dammy <bestbynature2010@gmail.com>
2023-05-12 15:36:22 +0100

    update the price tag component

--- src/components/PriceTag.tsx
+++ src/components/PriceTag.tsx
@@ -1,10 +1,8 @@
-import { FormatPrice } from "@/lib/format";
-import { PriceTagProps } from "@/lib/types/types";
+import { FormatPrice } from '@/lib/format';
+import { PriceTagProps } from '@/lib/types/types';
 
-const PriceTag = ({price, className}: PriceTagProps) => {
-  return (
-    <span className={`badge ${className}`}>{FormatPrice(price)}</span>
-  )
-}
+const PriceTag = ({ price, className }: PriceTagProps) => {
+  return <span className={`badge ${className}`}>{FormatPrice(price)}</span>;
+};
 
-export default PriceTag
+export default PriceTag;
