bd76360021a318e4db67080868f2b7de06b40f4d
Dammy <bestbynature2010@gmail.com>
2023-07-05 07:01:20 +0100

    Update the types

--- src/lib/types/types.ts
+++ src/lib/types/types.ts
@@ -1,24 +1,49 @@
+import { Prisma } from '@prisma/client';
+import { prisma } from '../db/prisma';
+
 export interface Product {
-  id: string
-  name: string
-  description: string
-  imageUrl: string
-  price: number
-  createdAt: Date
-  updatedAt: Date
+  id: string;
+  name: string;
+  description: string;
+  imageUrl: string;
+  price: number;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 export interface ProductCardProps {
-  products: Product[]
+  products: Product[];
 }
 
 export interface PriceTagProps {
-  price: number,
-  className?: string,
+  price: number;
+  className?: string;
 }
 
 export interface ProductPageProps {
   params: {
-    id: string
-  }
+    id: string;
+  };
 }
+
+export interface AddToCartButtonProps {
+  productId: string;
+  incrementProductQuantity: (productId: string) => Promise<void>;
+}
+
+export type CartWithProducts = Prisma.CartGetPayload<{
+  include: {
+    items: {
+      include: {
+        product: true;
+      };
+    };
+  };
+}>;
+
+export type ShoppingCart =
+  | (CartWithProducts & {
+      size: number;
+      subtotal: number;
+    })
+  | null;
